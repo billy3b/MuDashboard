22437f6ea896b9c6e73bfee4862cfec83e977ce1
mayankmusigma <mayankmusigma@gmail.com>
2024-10-08 16:51:54 +0530

    Update customers.jsx

--- src/component/pages/customers.jsx
+++ src/component/pages/customers.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Customer(){
     return(
         <div className="customer">
-            Customers
+            Customer
         </div>
     )
 }
