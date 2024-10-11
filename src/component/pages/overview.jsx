ce2a8d8dfe98ce78f76c83f39a46d13cc8d2a4c6
Mayank Srivastava <Mayank.Srivastava@mu-sigma.com>
2024-10-11 14:13:12 +0530

    fixed the bug

--- src/component/pages/overview.jsx
+++ src/component/pages/overview.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./overview.css";
 import CardTemp from "../cardtemp";
 import YearFilter from "../yearfilter";
@@ -10,22 +10,53 @@ export default function Overview(){
     const[profit, setProfit] = useState();
     const[aov, setAov] = useState();
     const [filteredData, setFilteredDate] = useState();
+    const hasMounted = useRef(false); // Ref to track first render
+    const hadMounted = useRef(false);
     const handleDropdownChange = (value) => {
         setYear(value);
       };
+   useEffect(() => {
+     const fd = dataJson.filter((item) => {
+         return item.Year ==="2015";
+       });
+       let plz = fd[0]
+       console.log("please",plz.Sales)
+       setSales(plz.Sales)
+       setProfit(plz.Profit)
+       setAov(plz.AOV)
+   },[])
       useEffect(() => {
-        const fd = dataJson.filter((item) => {
-            
-            return item.Year === year.toString();
-          });
-          setFilteredDate(fd);
+
+        if (hasMounted.current) {
+            const filtered = dataJson.filter(item => item.Year === year.toString());
+            console.log("filll",filtered)
+            setFilteredDate(filtered)
+            setSales(filtered[0].Sales)
+            setProfit(filtered[0].Profit)
+            setAov(filtered[0].AOV)
+
+          } else {
+            hasMounted.current = true; // Set to true after the first render
+        }
+
+        // const fd = dataJson.filter((item) => {
+     
+        //     return item.Year === year.toString();
+        //   });
+        //   setFilteredDate(fd);
+          
       },[year])
-      useEffect(() => {
+    //   useEffect(() => {
+    //     if(hadMounted.current){
+    //         console.log("dwdwd",filteredData)
+    //         setSales(filteredData[0].Sales)
+    //         setProfit(filteredData[0].Profit)
+    //         setAov(filteredData[0].AOV)
+    //     }else{
+    //         hadMounted.current=true;
+    //     }
         
-        setSales(filteredData[0].Sales)
-        setProfit(filteredData[0].Profit)
-        setAov(filteredData[0].AOV)
-      },[filteredData])
+    //   },[filteredData])
     return(
         <>
         <YearFilter handleDropdownChange={handleDropdownChange} className="year-top"/>
